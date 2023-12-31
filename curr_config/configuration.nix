eba84c520165a649e9c10945413a57caa7b7cc90
TrueBad0ur <pieceofcakecupofcoffee@gmail.com>
2023-12-31 17:43:27 +0300

    Add keystone, unzip

--- curr_config/configuration.nix
+++ curr_config/configuration.nix
@@ -144,7 +144,7 @@
   # $ nix search wget
   environment.systemPackages = with pkgs; [
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-    wget curl musikcube
+    wget curl musikcube unzip
     neofetch zsh oh-my-zsh alacritty vim
     docker firefox git tdesktop htop tmux file feh xclip
     minikube kubernetes-helm terraform wireguard-tools jq
