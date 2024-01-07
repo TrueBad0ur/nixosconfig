3b9f78fd8c135175a86a1fa9491044221adc04a1
TrueBad0ur <pieceofcakecupofcoffee@gmail.com>
2024-01-07 18:13:07 +0300

    Add wine and local config for rogauracore

--- asusrogzephyrusg14_config/buildrog/static.nix
+++ asusrogzephyrusg14_config/buildrog/static.nix
@@ -1,5 +1,9 @@
-with import <nixpkgs> {};
-
+{
+  pkgs ? import (fetchTarball {
+    url = "https://github.com/NixOS/nixpkgs/archive/4fe8d07066f6ea82cda2b0c9ae7aee59b2d241b3.tar.gz";
+    sha256 = "sha256:06jzngg5jm1f81sc4xfskvvgjy5bblz51xpl788mnps1wrkykfhp";
+  }) {}
+}:
 pkgs.stdenv.mkDerivation rec {
   pname = "rogauracore";
   version = "1.6.0";
@@ -13,8 +17,6 @@ pkgs.stdenv.mkDerivation rec {
   buildInputs = [
     pkgs.libusb1
     pkgs.autoreconfHook
-    #pkgs.boost
-    #pkgs.cmake
   ];
 
   configurePhase = ''
