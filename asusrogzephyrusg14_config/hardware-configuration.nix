1515915d445a06e0daa030bce530c0b452305a60
Truebad0ur <pieceofcakecupofcoffee@gmail.com>
2024-02-03 13:42:37 +0300

    Ability to use theme in grub

--- asusrogzephyrusg14_config/hardware-configuration.nix
+++ asusrogzephyrusg14_config/hardware-configuration.nix
@@ -16,11 +16,24 @@
       grub = {
         enable = true;
 	
+        #theme = pkgs.stdenv.mkDerivation {
+        #  pname = "distro-grub-themes";
+        #  version = "3.1";
+        #  src = pkgs.fetchFromGitHub {
+        #    owner = "AdisonCavani";
+        #    repo = "distro-grub-themes";
+        #    rev = "v3.1";
+        #    hash = "sha256-ZcoGbbOMDDwjLhsvs77C7G7vINQnprdfI37a9ccrmPs=";
+        #  };
+        #  installPhase = "cp -r customize/nixos $out";
+        #};
+
         efiSupport = true;
         efiInstallAsRemovable = true;
         device = "nodev";
-	#extraEntriesBeforeNixOS = true;
         splashImage = ./grub.png;
+
+        #extraEntriesBeforeNixOS = true;
         #extraEntries = ''
         #  menuentry "Reboot" {
         #    reboot
