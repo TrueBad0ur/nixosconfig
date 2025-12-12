11fe9b9214897f4c592875b414adadc04c193573
TrueBad0ur <pieceofcakecupofcoffee@gmail.com>
2025-12-13 02:19:38 +0300

    Changed copy

--- server/nixos/modules/zsh.nix
+++ server/nixos/modules/zsh.nix
@@ -9,7 +9,7 @@
     shellAliases = {
       rebuild = "sudo nixos-rebuild switch --flake /etc/nixos";
       customrebuild = "sudo nixos-rebuild -I nixpkgs=/home/truebad0ur/nixpkgs switch";
-      copy = "sudo cp -r /etc/nixos/\* /home/truebad0ur/nixosconfig/server/ && sudo chown -R truebad0ur:users /home/truebad0ur/nixosconfig/server/";
+      copy = "sudo cp -r /etc/nixos/\* /home/truebad0ur/nixosconfig/server/ && sudo cat /home/truebad0ur/README.md > /home/truebad0ur/nixosconfig/commands.txt && sudo chown -R truebad0ur:users /home/truebad0ur/nixosconfig/server/";
       list-generations = "sudo nix-env --list-generations --profile /nix/var/nix/profiles/system";
     };
     autosuggestions.enable = true;
